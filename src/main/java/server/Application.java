ac3332b76c5731ee9023eadb5c1d4800163146a7
Vibhav Srivastava <vibhav@terrafin.tech>
2024-06-19 11:41:11 +0530

    moving to spring 3.2.6 and java 21

--- src/main/java/server/Application.java
+++ src/main/java/server/Application.java
@@ -1,4 +1,4 @@
-package server.healthcheck;
+package server;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
